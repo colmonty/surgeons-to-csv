f6926376f53d92b00e73824f075d0a86a5949f62
Colin Montgomery <col.monty@gmail.com>
2016-10-08 18:16:46 +0100

    Relative path for saved csv file

--- surgeons.ps1
+++ surgeons.ps1
@@ -1,5 +1,8 @@
 ﻿$sourceUrl = "http://njrsurgeonhospitalprofile.org.uk/SurgeonListing/GetPageData?letter=%25"
+$destinationFilePath = $PSScriptRoot + "\surgeons.csv"
+Write-Host $destinationFilePath
 
+Write-Host "Getting source content..."
 $rawContent = (Invoke-WebRequest -Uri $sourceUrl).Content
 $trimmedContent = $rawContent.TrimStart('"').TrimEnd('"')
 
@@ -21,10 +24,13 @@ Content-Disposition: form-data; name="inputString"
 
 $body = $template -F $trimmedContent
 $unescapeUri = "http://www.freeformatter.com/string-escaper.html"
+Write-Host "Sending unquoted content to be un-escaped..."
 $escapedResponse = Invoke-WebRequest -Uri $unescapeUri `
                                      -Method Post `
                                      -Body $body `
                                      -ContentType "multipart/form-data; boundary=----WebKitFormBoundaryAoNdfgM0uhQhLVtH"
-                                    
+
+Write-Host "Parsing the content, converting to CSV and saving..."                               
 $psObject = $escapedResponse.ParsedHtml.getElementsByTagName('textarea')[1].Value | ConvertFrom-Json
-$psObject | Export-Csv 'C:\src\surgeons\surgeons.csv' -NoTypeInformation
+$psObject | Export-Csv $destinationFilePath -NoTypeInformation
+Write-Host "Complete!  File surgeons.csv saved to disk"
