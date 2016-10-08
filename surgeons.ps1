005e036794e97dbcc983bd006ee2de598680b0b7
Colin Montgomery <col.monty@gmail.com>
2016-10-08 17:21:18 +0100

    Initial version of ps script

---
+++ surgeons.ps1
@@ -0,0 +1,30 @@
+﻿$sourceUrl = "http://njrsurgeonhospitalprofile.org.uk/SurgeonListing/GetPageData?letter=%25"
+
+$rawContent = (Invoke-WebRequest -Uri $sourceUrl).Content
+$trimmedContent = $rawContent.TrimStart('"').TrimEnd('"')
+
+$template = @'
+------WebKitFormBoundaryAoNdfgM0uhQhLVtH
+Content-Disposition: form-data; name="action"
+
+unescape
+------WebKitFormBoundaryAoNdfgM0uhQhLVtH
+Content-Disposition: form-data; name="target"
+
+javascript
+------WebKitFormBoundaryAoNdfgM0uhQhLVtH
+Content-Disposition: form-data; name="inputString"
+
+{0}
+------WebKitFormBoundaryAoNdfgM0uhQhLVtH--
+'@
+
+$body = $template -F $trimmedContent
+$unescapeUri = "http://www.freeformatter.com/string-escaper.html"
+$escapedResponse = Invoke-WebRequest -Uri $unescapeUri `
+                                     -Method Post `
+                                     -Body $body `
+                                     -ContentType "multipart/form-data; boundary=----WebKitFormBoundaryAoNdfgM0uhQhLVtH"
+                                    
+$psObject = $escapedResponse.ParsedHtml.getElementsByTagName('textarea')[1].Value | ConvertFrom-Json
+$psObject | Export-Csv 'C:\src\surgeons\surgeons.csv' -NoTypeInformation
